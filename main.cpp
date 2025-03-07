930e46b42ad304d88de5aab82d110189d3568d36
newagebegins <newagebegins@gmail.com>
2025-03-07 17:48:31 +1000

    Display a window with GLFW

--- main.cpp
+++ main.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
+static void errorCallback(int /*error*/, const char* description)
+{
+    std::cerr << "GLFW error: " << description << '\n';
+}
+
 int main()
 {
-    std::cout << "Hello, World!\n";
+    glfwSetErrorCallback(errorCallback);
+
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW\n";
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
+
+    GLFWwindow* window{ glfwCreateWindow(600, 800, "my_pinball", nullptr, nullptr) };
+    if (!window)
+    {
+        std::cerr << "Failed to create GLFW window\n";
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        {
+            glfwSetWindowShouldClose(window, true);
+        }
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
     return 0;
 }
 
