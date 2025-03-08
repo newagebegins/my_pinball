ad18cc1d4b5a639dd162c456df98e02bb21e2448
newagebegins <newagebegins@gmail.com>
2025-03-09 08:33:26 +1000

    Add OpenGL debug output

--- main.cpp
+++ main.cpp
@@ -8,6 +8,55 @@ static void errorCallback(int /*error*/, const char* description)
     std::cerr << "GLFW error: " << description << '\n';
 }
 
+void APIENTRY glDebugOutput(
+    GLenum source,
+    GLenum type,
+    GLuint id,
+    GLenum severity,
+    GLsizei /*length*/,
+    const GLchar* message,
+    const void* /*userParam*/)
+{
+    std::cerr << "OpenGL debug message (" << id << "): " << message << '\n';
+
+    switch (source)
+    {
+    case GL_DEBUG_SOURCE_API:             std::cerr << "Source: API"; break;
+    case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cerr << "Source: Window System"; break;
+    case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cerr << "Source: Shader Compiler"; break;
+    case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cerr << "Source: Third Party"; break;
+    case GL_DEBUG_SOURCE_APPLICATION:     std::cerr << "Source: Application"; break;
+    case GL_DEBUG_SOURCE_OTHER:           std::cerr << "Source: Other"; break;
+    default:                              std::cerr << "Source: ???"; break;
+    }
+    std::cerr << '\n';
+
+    switch (type)
+    {
+    case GL_DEBUG_TYPE_ERROR:               std::cerr << "Type: Error"; break;
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cerr << "Type: Deprecated Behaviour"; break;
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cerr << "Type: Undefined Behaviour"; break;
+    case GL_DEBUG_TYPE_PORTABILITY:         std::cerr << "Type: Portability"; break;
+    case GL_DEBUG_TYPE_PERFORMANCE:         std::cerr << "Type: Performance"; break;
+    case GL_DEBUG_TYPE_MARKER:              std::cerr << "Type: Marker"; break;
+    case GL_DEBUG_TYPE_PUSH_GROUP:          std::cerr << "Type: Push Group"; break;
+    case GL_DEBUG_TYPE_POP_GROUP:           std::cerr << "Type: Pop Group"; break;
+    case GL_DEBUG_TYPE_OTHER:               std::cerr << "Type: Other"; break;
+    default:                                std::cerr << "Type: ???"; break;
+    }
+    std::cerr << '\n';
+
+    switch (severity)
+    {
+    case GL_DEBUG_SEVERITY_HIGH:         std::cerr << "Severity: high"; break;
+    case GL_DEBUG_SEVERITY_MEDIUM:       std::cerr << "Severity: medium"; break;
+    case GL_DEBUG_SEVERITY_LOW:          std::cerr << "Severity: low"; break;
+    case GL_DEBUG_SEVERITY_NOTIFICATION: std::cerr << "Severity: notification"; break;
+    default:                             std::cerr << "Severity: ???"; break;
+    }
+    std::cerr << "\n\n";
+}
+
 int main()
 {
     glfwSetErrorCallback(errorCallback);
@@ -36,6 +85,11 @@ int main()
         std::cerr << "Failed to initialize GLAD\n";
         return 1;
     }
+
+    glEnable(GL_DEBUG_OUTPUT);
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+    glDebugMessageCallback(glDebugOutput, nullptr);
+    glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
 
     while (!glfwWindowShouldClose(window))
     {
