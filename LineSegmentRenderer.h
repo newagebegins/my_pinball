81d565e347d9464b531ab21167c50dffce4e2b2c
newagebegins <newagebegins@gmail.com>
2025-03-19 20:28:12 +1000

    Inline LineSegmentRenderer.cpp

--- LineSegmentRenderer.h
+++ LineSegmentRenderer.h
@@ -10,8 +10,19 @@
 class LineSegmentRenderer
 {
 public:
-    LineSegmentRenderer(const std::vector<DefaultVertex>& verts);
-    void render(const DefaultShader& s) const;
+    LineSegmentRenderer(const std::vector<DefaultVertex>& verts)
+        : m_vao{ DefaultShader::createVao(verts) }
+        , m_numVerts{ static_cast<int>(verts.size()) }
+    {}
+
+    void render(const DefaultShader& s) const
+    {
+        s.setModel({ 1.0f });
+    
+        glBindVertexArray(m_vao);
+        glDrawArrays(GL_LINES, 0, m_numVerts);
+    }
+
 private:
     const GLuint m_vao{};
     const int m_numVerts{};
