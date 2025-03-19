d0432752be3557d52677447d432a58a254406d6f
newagebegins <newagebegins@gmail.com>
2025-03-20 08:00:39 +1000

    Inline Arc.h

--- Game.h
+++ Game.h
@@ -1,17 +1,33 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include "Arc.h"
 #include "Circle.h"
 #include "DefaultVertex.h"
 #include "Flipper.h"
+#include "Math.h"
 
 #include <glm/glm.hpp>
 
+#include <cassert>
 #include <vector>
 
 #define BUTTON_L (1 << 0)
 #define BUTTON_R (1 << 1)
+
+struct Arc
+{
+    glm::vec2 p;
+    float r;
+    float start;
+    float end;
+
+    Arc(glm::vec2 P, float R, float S, float E)
+        : p{P}, r{R}, start{S}, end{E}
+    {
+        assert(0.0f <= start && start < twoPi);
+        assert(0.0f <= end && end < twoPi);    
+    }
+};
 
 struct Game
 {
